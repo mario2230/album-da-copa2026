da82d8b3bb7fdbb04937d7a7f0f0cdadf55d9d2d
mario2230 <124641210+mario2230@users.noreply.github.com>
2026-06-03 14:06:39 -0300

    innit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'album-da-copa2026',
+  webDir: 'dist'
+};
+
+export default config;
